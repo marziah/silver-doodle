858d84ebd2124b3a621987165ab55926ce847beb
marziah <marziah@gmail.com>
2018-10-03 20:25:21 -0700

    added counter

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+
+//var x =12;
+//if (x % 2 ===0) {
+ //   console.log(x , 'is even');
+//}
+//else {
+//    console.log(x, 'is odd');
+
+//} 
+for (var y = 0; y < 101; y++){
+    if ( y % 2 ===0){
+        console.log('the nunber is', y);
+    }
+     
+    
+}
