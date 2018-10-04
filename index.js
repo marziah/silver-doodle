20c3b178789e4ab7353b53ed42ef83d6a3a726a3
marziah <marziah@gmail.com>
2018-10-03 20:42:55 -0700

    added prompt

--- index.js
+++ index.js
@@ -7,7 +7,10 @@
 //    console.log(x, 'is odd');
 
 //} 
-for (var y = 0; y < 101; y++){
+
+var CountTo = prompt('How high should we count?')
+var y = 1;
+for (var y = 0; y < CountTo;  y++){
     if ( y % 2 ===0){
         console.log('the even number is', y);
     }
